0b36f2a1367a8b1adc8b0e5e2051bacb27f818e1
Dongheon, Kim <oueya1479@naver.com>
2021-03-13 23:59:33 +0900

    Initial commit

--- project/src/dto/Customer.java
+++ project/src/dto/Customer.java
@@ -9,18 +9,9 @@ public class Customer {
 	private String cstmName; //회원이름
 	private String tel;      //연락처
 	private int mileage;     //마일리지
-<<<<<<< HEAD
 	private String pname;  //반려견이름
 	private Double pweight;   //몸무게
 	private String pbreed; //견종
-	
-	Customer(){}
-	
-
-	public Customer(int cardno, String id, String password, String cstmName, String tel, int mileage, String pname,
-			Double pweight, String pbreed) {
-=======
-	private Puppy puppy;
 	
 
 	// 회원가입시 회원아이디, 비밀번호, 이름, 연락처만 받는 생성자
@@ -31,8 +22,9 @@ public class Customer {
 		this.tel = tel;
 	}
 
-	public Customer(int cardno, String id, String password, String cstmName, String tel, int mileage, Puppy puppy) {
->>>>>>> 4a9a50dc5064d430575d01764c18cbeb748d5d68
+
+	public Customer(int cardno, String id, String password, String cstmName, String tel, int mileage, String pname,
+			Double pweight, String pbreed) {
 		super();
 		this.cardno = cardno;
 		this.id = id;
@@ -40,13 +32,9 @@ public class Customer {
 		this.cstmName = cstmName;
 		this.tel = tel;
 		this.mileage = mileage;
-<<<<<<< HEAD
 		this.pname = pname;
 		this.pweight = pweight;
 		this.pbreed = pbreed;
-=======
-		this.puppy = puppy;
->>>>>>> 4a9a50dc5064d430575d01764c18cbeb748d5d68
 	}
 
 
@@ -128,7 +116,6 @@ public class Customer {
 	public void setMileage(int mileage) {
 		this.mileage = mileage;
 	}
-<<<<<<< HEAD
 
 
 
@@ -167,18 +154,6 @@ public class Customer {
 	}
 
 
-
-=======
-	
-	public Puppy getPuppy() {
-		return puppy;
-	}
-
-	public void setPuppy(Puppy puppy) {
-		this.puppy = puppy;
-	}
-
->>>>>>> 4a9a50dc5064d430575d01764c18cbeb748d5d68
 	@Override
 	public String toString() {
 		return "-------------------------------고객정보--------------------------------------"
--- project/src/view/menu/MainView.java
+++ project/src/view/menu/MainView.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package view.menu;
 
 import java.util.Scanner;
@@ -40,46 +39,3 @@ public class MainView {
 		mv.printMenu();
 	}
 }
-=======
-package view.menu;
-
-import java.util.Scanner;
-
-import view.login.JoinView;
-import view.login.LoginView;
-
-public class MainView {
-	MainView(){}
-	Scanner sc = new Scanner(System.in);
-	LoginView login = new LoginView();
-	JoinView join = new JoinView();
-	
-	public void printMenu(){
-	while(true) {
-		System.out.println("------------------------------------------------------");
-		System.out.println("               Welcome to PODO SALON");
-		System.out.println("------------------------------------------------------");
-		System.out.println("1. 로그인  2. 회원가입  3.아이디/비밀번호 찾기  0. 프로그램 종료");
-		System.out.println("------------------------------------------------------");
-		int menuChoice = Integer.parseInt(sc.nextLine());
-		switch(menuChoice) {
-		case 1 : login.loginstart(); break;
-		case 2 : join.joinStart(); break;
-		case 3 :
-		case 0 : 
-			System.out.println("프로그램 종료.");
-			System.exit(0);
-			break;
-			default : System.out.println("메뉴선택 오류입니다. 다시 선택해주세요");
-			
-		}
-	
-	}
-	
-	}
-	public static void main(String[] args) {
-		MainView mv = new MainView();
-		mv.printMenu();
-	}
-}
->>>>>>> 4a9a50dc5064d430575d01764c18cbeb748d5d68
